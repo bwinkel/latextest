e2cea80f7d86a0fade31e5fffdb781564a6bece5
Benjamin Winkel <bwinkel@users.noreply.github.com>
2021-06-21 13:15:54 +0200

    Update compile_to_pdf.sh

--- actions/pdflatex/compile_to_pdf.sh
+++ actions/pdflatex/compile_to_pdf.sh
@@ -3,9 +3,14 @@
 set -e
 mkdir -p $ARTIFACTS_DIR
 
+# produce pdf
 pdflatex $TEXFILE
 # bibtex $TEXFILE
 # pdflatex $TEXFILE
 # pdflatex $TEXFILE
 
+# produce html pages
+latex2html $TEXFILE -mkdir -dir html
+
 cp main.pdf $ARTIFACTS_DIR
+cp -ar html/* $ARTIFACTS_DIR
